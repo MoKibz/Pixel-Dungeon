45457e03356ee3b15e5278a5d009babfbc24c81c
MoKibz <121356941+MoKibz@users.noreply.github.com>
2023-11-02 11:37:11 +0000

    change 7

--- Characters.py
+++ Characters.py
@@ -4,21 +4,32 @@ class Characters:
 
     def __init__(self):
         self._Alive = True
+        self.level = 1
         self.movement_speed = 10
         self.Type = ['close_range', 'long_range']
-        self.attack_range = {self.Type[0]: 20,
-                             self.Type[1]: 200}
+        self.attack_range = {self.Type[0]: 30,
+                             self.Type[1]: 210}
         self.defence = 100
         self.health = 100
+        self.Weapons = ['gun', 'sword']
+        self.Potions = ['health_potion', 'defence_potion']
 
 class Shooter(Characters):
-
     def __init__(self):
         super().__init__()
+        self.lvl = self.level
         self.Character_Type = self.attack_range[self.Type[1]]
         self.Stats = [self.health, (self.defence - 20)]
         self.speed = self.movement_speed - 2
+        self.Shooter_item = [self.Weapons[1], ]
 
+class Melee(Characters):
+    def __init__(self):
+        super().__init__()
+        self.lvl = self.level
+        self.Character_Type = self.attack_range[self.Type[0]]
+        self.Stats = [self.health, (self.defence + 10)]
+        self.speed = self.movement_speed + 5
 
 
 
